c4adcd5bf8fdd6883e3c12bcfbcea740912c467f
Toffee <Toffee@Transfrm.com>
2023-02-19 14:49:11 +0700

    Add comment in form1 for understandability

--- NestedFlowchart/Form1.cs
+++ NestedFlowchart/Form1.cs
@@ -57,34 +57,41 @@ namespace NestedFlowchart
             {
                 Transfrom transfrom = new();
 
-                //Read XML Flowchart
+                #region Read XML Flowchart
                 var allFlowChartElements = transfrom.Extracttion(this.txt_InputPath);
+                #endregion
 
-                //Sorted Flowchart Element
-                //input : allFlowchartElement
-                //output : sortedFlowchart
-                //Iterate until nextElement is null
+                #region Sorted by finding the nextid of each element
 
+                //Create Temp variable for store another direction of decision node
                 List<XMLCellNode> tempDecisionElements = new List<XMLCellNode>();
 
+                //Find a start node
                 var startElement = allFlowChartElements.Find(x => x.ValueText.ToLower() == "start");
                 sortedFlowcharts.Add(startElement);
 
+                //Finding next id after start node (Initialization process)
                 XMLCellNode? nextElement = transfrom.SortedFlowchartElement(allFlowChartElements, sortedFlowcharts, tempDecisionElements, startElement);
                 while (nextElement != null)
                 {
+                    //Find next node
                     nextElement = transfrom.SortedFlowchartElement(allFlowChartElements, sortedFlowcharts, tempDecisionElements, nextElement);
                 }
-                sortedFlowcharts.Remove(null);
 
-                //Add column NodeType
+                //Removing the null the end of the node
+                sortedFlowcharts.Remove(null);
+                #endregion
+
+                #region Add column NodeType
                 foreach (var s in sortedFlowcharts)
                 {
                     s.NodeType = transfrom.CheckFCNodeType(s);
                 }
+                #endregion
 
-                //Bind to datasource table after formatted
+                #region Bind to datasource table after formatted
                 dg_ElementPreview.DataSource = sortedFlowcharts;
+                #endregion
             }
             catch (Exception ex)
             {
@@ -96,13 +103,16 @@ namespace NestedFlowchart
         {
             try
             {
+                #region Read all template
                 var TemplatePath = ConfigurationManager.AppSettings["TemplatePath"];
                 var ResultPath = ConfigurationManager.AppSettings["ResultPath"];
+                #endregion
 
-                //Write Result Path
+                #region Write Result Path
                 txt_ReultPath.Text = ResultPath;
+                #endregion
 
-                //Export to CPN Tools file
+                #region Export to CPN Tools file
                 Rule1 rule1 = new Rule1();
                 Rule2 rule2 = new Rule2();
                 Rule3 rule3 = new Rule3();
@@ -114,6 +124,7 @@ namespace NestedFlowchart
 
                 var exportToCPN = new ExportToCPN(rule1, rule2, rule3, rule4, rule5, rule6, rule7, approach);
                 exportToCPN.ExportFile(TemplatePath, ResultPath, sortedFlowcharts);
+                #endregion
             }
             catch (Exception ex)
             {
