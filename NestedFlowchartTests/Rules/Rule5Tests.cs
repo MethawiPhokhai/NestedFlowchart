eb6ef95aa80efa0eccc82535c4d44b0ce193a9d9
Toffee <Toffee@transfrm.com>
2023-04-26 22:45:03 +0700

    Unit Test for Rule5

--- NestedFlowchartTests/Rules/Rule5Tests.cs
+++ NestedFlowchartTests/Rules/Rule5Tests.cs
@@ -7,36 +7,45 @@ namespace NestedFlowchart.Rules.Tests
     [TestClass()]
     public class Rule5Tests
     {
-        //[TestMethod()]
-        //public void ApplyRule_ValidInput_ReturnsExpectedOutput()
-        //{
-        //    // Arrange
-        //    PositionManagements page1Position = new PositionManagements();
+        [TestMethod()]
+        public void ApplyRule_ValidInput_ReturnsExpectedOutput()
+        {
+            // Arrange
+            PositionManagements page1Position = new PositionManagements();
 
-        //    PreviousNode previousNode = new PreviousNode();
-        //    previousNode.currentPlaceModel = new PlaceModel()
-        //    {
-        //        Id1 = "ID1412948772",
-        //        Id2 = "ID1412948773",
-        //        Id3 = "ID1412948774",
-        //        Name = "Start",
-        //        Type = "UNIT",
-        //        xPos1 = page1Position.xPos1,
-        //        yPos1 = page1Position.yPos1,
-        //        xPos2 = page1Position.xPos2,
-        //        yPos2 = page1Position.yPos2,
-        //        xPos3 = page1Position.xPos3,
-        //        yPos3 = page1Position.yPos3
-        //    };
-        //    var arrayName = "array";
-        //    int countSubPage = 0;
+            List<PreviousNode> previousNodes = new List<PreviousNode>();
+            PreviousNode previousNode = new PreviousNode
+            {
+                currentPlaceModel = new PlaceModel()
+                {
+                    Id1 = "ID1412948772",
+                    Id2 = "ID1412948773",
+                    Id3 = "ID1412948774",
+                    Name = "Start",
+                    Type = "UNIT",
+                    xPos1 = page1Position.xPos1,
+                    yPos1 = page1Position.yPos1,
+                    xPos2 = page1Position.xPos2,
+                    yPos2 = page1Position.yPos2,
+                    xPos3 = page1Position.xPos3,
+                    yPos3 = page1Position.yPos3
+                }
+            };
 
-        //    // Act
-        //    Rule5 rule5 = new Rule5();
-        //    var (pl, tr, a1, a2) = rule5.ApplyRule(arrayName, previousNode, page1Position, countSubPage);
+            previousNodes.Add(previousNode);
 
-        //    // Assert
-        //    Assert.IsTrue(true);
-        //}
+            var arrayName = "array";
+
+            // Act
+            Rule5 rule5 = new Rule5();
+            var (rule5Place, rule5Transition, rule5Arc1, previousTypeReturn) = rule5.ApplyRule(
+                            arrayName,
+                            previousNodes.LastOrDefault(),
+                            page1Position,
+                            0);
+
+            // Assert
+            Assert.IsNotNull(rule5Place);
+        }
     }
 }
